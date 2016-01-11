d989d0b3d7e8c15e17da42986f01902a7ce0b87a
gitrom19 <grorom@gmx.at>
2016-01-11 15:01:51 +0100

    added some functionality to the rest service (spring boot tutorial)

--- springRestService/src/main/java/hello/Application.java
+++ springRestService/src/main/java/hello/Application.java
@@ -1,12 +1,23 @@
 package hello;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		//		SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        Stream.of(beanNames).forEach(System.out::println);
 	}
 }
--- springRestService/src/main/java/hello/GreetingController.java
+++ springRestService/src/main/java/hello/GreetingController.java
@@ -10,13 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(path="/{greeting}", method=RequestMethod.GET)
-//    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@RequestMapping(path = "/{greeting}", method = RequestMethod.GET)
+	// @RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 }
